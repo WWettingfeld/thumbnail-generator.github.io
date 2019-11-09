11df849414870b49f9a983ac2e48c1083a56482b
William Wettingfeld <bwett01@gmail.com>
2019-11-09 16:15:24 -0600

    Cleaned up logic more

--- js/index.js
+++ js/index.js
@@ -1,30 +1,22 @@
-var matches = [];
+// MNM:209:hamyojo:Sheik:Arsenals:Fox:Winners:Melee
 
-$(document).ready(function() {
-	$("#thumbnail").css("font-family", "Tahoma");
+function createThumbnails() {
+	var matches = getMatches();
+	setBaseCss();
 	
-	$(document).on("click", "#btn-download", function() {
-		parseInput();
-		determineCss();
-		
-		var i = 0;
-		setInterval(function() {
-			if (matches[i]) {
-				injectMatchInfo(matches[i]);
-				renderAndSave();
-				i++;
-			}		
-		}, 5000);
-	});
-});
+	var i = 0;
+	setInterval(function() {
+		if (matches[i]) {
+			injectMatchIntoHtml(matches[i]);
+			renderAndSave();
+			i++;
+		}		
+	}, 5000);
+}
 
+function getMatches() {
+	var matches = [];
 
-// MNM		:209	:hamyojo:Sheik	:Arsenals	:Fox	:Winners	:Melee
-// event	:num	:p1		:c1		:p2			:c2		:bracket	:game
-// 0		:1		:2		:3		:4			:5		:6			:7		
-function parseInput() {
-	matches = [];
-	
 	var input = $("#input").val();
 	var	splitMatches = input.split('\n');
 
@@ -34,7 +26,7 @@ function parseInput() {
 			event: splitMatch[0],
 			number: splitMatch[1],
 			player1: splitMatch[2],
-			char1: splitMatch[3],
+			char1: splitmatch.char1,
 			player2: splitMatch[4],
 			char2: splitMatch[5],
 			bracket: splitMatch[6],
@@ -43,35 +35,37 @@ function parseInput() {
 
 		matches.push(match);
 	}
+
+	return matches;
 }
 
-function determineCss() {
+function setBaseCss() {
 	$('link[href="css/index.css"]').attr('href','css/index.css');
 }
 
-function injectMatchInfo(match) {
+function injectMatchIntoHtml(match) {
 	setFoundationImages(match);
-	var game = setGameLogo(match);
+	setGameLogo(match);
 	setEventCss(match);
 	setText(match);
-	setCharacterPortraits(match, game);
+	setCharacterPortraits(match);
 }
 
 function setFoundationImages(match) {
-	$("#background").attr("src", "img/event/" + match[0] + "/background.png");
-	$("#foreground").attr("src", "img/event/" + match[0] + "/foreground.png");
-	$("#gameLogo").attr("src", "img/game/" + match[7] + ".png");
+	$("#background").attr("src", "img/event/" + match.event + "/background.png");
+	$("#foreground").attr("src", "img/event/" + match.event + "/foreground.png");
+	$("#gameLogo").attr("src", "img/game/" + match.game + ".png");
 }
 
 function setText(match) {
-	$("#number").html(match[1]);
-	$("#p1Name").html(match[2]);
-	$("#p2Name").html(match[4]);
-	$("#bracket").html(match[6]);
+	$("#number").html(match.number);
+	$("#p1Name").html(match.player1);
+	$("#p2Name").html(match.player2);
+	$("#bracket").html(match.bracket);
 }
 
-function setCharacterPortraits(match, game) {
-	switch(match[7].toLowerCase()){
+function setCharacterPortraits(match) {
+	switch(match.game.toLowerCase()){
 		case "ssbu":
 		case "samurai":
 		case "samuraishowdown":
@@ -80,48 +74,43 @@ function setCharacterPortraits(match, game) {
 		case "tekken":
 		case "tekken7":
 		case "tekken 7":
-			$("#p1Char").attr("src", "img/chars/" + game + "/p1/" + match[3] + ".png");
-			$("#p2Char").attr("src", "img/chars/" + game + "/p2/" + match[5] + ".png");
+			$("#p1Char").attr("src", "img/chars/" + match.game + "/p1/" + match.char1 + ".png");
+			$("#p2Char").attr("src", "img/chars/" + match.game + "/p2/" + match.char2 + ".png");
 			break;
 		default:
-			$("#p1Char").attr("src", "img/chars/" + game +  "/" + match[3] + ".png");
+			$("#p1Char").attr("src", "img/chars/" + match.game +  "/" + match.char1 + ".png");
 			$("#p2Char").css("transform", "scaleX(-1)");
-			$("#p2Char").attr("src", "img/chars/" + game +  "/" + match[5] + ".png");
+			$("#p2Char").attr("src", "img/chars/" + match.game +  "/" + match.char2 + ".png");
 			break;
 	}
 
-	if (match[7].toLowerCase() == "ssbu") {
-		$("#p1Char").attr("src", "img/chars/" + game + "/p1/" + match[3] + ".png");
+	if (match.game.toLowerCase() == "ssbu") {
+		$("#p1Char").attr("src", "img/chars/" + match.game + "/p1/" + match.char1 + ".png");
 		$("#p2Char").css("transform", "scaleX(1)");
-		$("#p2Char").attr("src", "img/chars/" + game + "/p2/" + match[5] + ".png");
+		$("#p2Char").attr("src", "img/chars/" + match.game + "/p2/" + match.char2 + ".png");
 	} 
 	else {
-		$("#p1Char").attr("src", "img/chars/" + game +  "/" + match[3] + ".png");
+		$("#p1Char").attr("src", "img/chars/" + match.game +  "/" + match.char1 + ".png");
 		$("#p2Char").css("transform", "scaleX(-1)");
-		$("#p2Char").attr("src", "img/chars/" + game +  "/" + match[5] + ".png");
+		$("#p2Char").attr("src", "img/chars/" + match.game +  "/" + match.char2 + ".png");
 	}
 }
 
 function setGameLogo(match) {
-	var game;
-
-	switch(match[7].toLowerCase()) {
+	switch(match.game.toLowerCase()) {
 		case "tekken 7":
 		case "tekken7":
 		case "tekken":
-			game = "tekken";
 			$("#gameLogo").attr("src", "img/game/tekken.png");
 			break;
 		case "samurai shodown":
 		case "samuraishodown":
 		case "samurai":
-			game = "samurai";
 			$("#gameLogo").attr("src", "img/game/samurai.png");
 			break;
 		case "slapcity":
 		case "slap":
 		case "slap city":
-			game = "Slap";
 			$("#gameLogo").attr("src", "img/game/Slap.png");
 			break;
 		case "mk11":
@@ -129,28 +118,20 @@ function setGameLogo(match) {
 		case "mk":
 		case "mortal kombat":
 		case "mortal kombat 11":
-			game = "mk11";
 			$("#gameLogo").attr("src", "img/game/MK11.png");
 			break;
 		case "brawl":
-			game = "PM";
 			$("#gameLogo").attr("src", "img/game/Brawl.png");
 			break;
 		case "p+":
 		case "pplus":
-			game = "PM";
 			$("#gameLogo").attr("src", "img/game/P+.png");
 			break;
-		default:
-			game = match[7];
-			break;
 	}
-
-	return game;
 }
 
 function setEventCss(match) {
-	switch(match[0].toLowerCase()){
+	switch(match.event.toLowerCase()){
 		case "mnm":
 		case "sw":
 		case "iab":
@@ -167,7 +148,7 @@ function setEventCss(match) {
     			"text-align": "left",
     			"margin-left": "0"
 			});
-			$("#gameLogo").attr("src", "img/game/" + match[7] + ".png");
+			$("#gameLogo").attr("src", "img/game/" + match.game + ".png");
 			break;
 		case "xdl":
 			$("#bracket").css("top", "218px");
@@ -229,4 +210,10 @@ function renderAndSave() {
 		}
 	});
 }
+
+function wireEvents() {
+	$(document).on("click", "#btn-download", createThumbnails);
+}
+
+$(document).ready(wireEvents);
 
